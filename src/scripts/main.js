f9ddbe7b3146fe9f67fd17f277c20d43c4a51ee1
mariana-martins <mariana_martins@id.uff.br>
2017-04-03 13:07:58 +1200

    Load data from Zomato API into Knockout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,32 @@
-
-var callback = function (error, data) {
-  if (error) {
-      console.log("Credo!!!");
-      return;
-  }
-
-  console.log(data);
+var Restaurant = function (data) {
+    this.name = ko.observable(data.name);
+    this.url = ko.observable(data.url);
+    this.address = ko.observable(data.location.address);
+    this.lat = ko.observable(data.location.latitude);
+    this.lon = ko.observable(data.location.longitude);
+    this.rating = ko.observable(data.user_rating.aggregate_rating);
 };
 
-zomatoApi.getRestaurants(callback);
+var ViewModel = function () {
+    self = this;
+
+    // TODO: CAPTURAR O EVENTO DE CLICK NA LISTA
+    // TODO: FILTRAR ELEMENTOS DA LISTA
+
+    this.restaurantList = ko.observableArray([]);
+
+    var zomatoApiCallback = function (error, data) {
+        if (error) {
+            alert("Error: Can't access Zomato API");
+            return;
+        }
+        data.restaurants.forEach(function (restaurantItem) {
+            self.restaurantList.push( new Restaurant(restaurantItem.restaurant));
+        });
+    };
+
+    zomatoApi.getRestaurants(zomatoApiCallback);
+
+};
+
+ko.applyBindings(new ViewModel());
